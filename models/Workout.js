b87b7e2c9864edeb2d6f1656a143310227a5a8b7
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-30 14:31:43 -0700

    hadn't included sets field in schema, overlooked locally bc seed data

--- models/Workout.js
+++ models/Workout.js
@@ -23,6 +23,9 @@ const workoutSchema = new Schema({
             reps: {
                 type: Number
             },
+            sets: {
+                type: Number
+            },
             distance: {
                 type: Number
             },
